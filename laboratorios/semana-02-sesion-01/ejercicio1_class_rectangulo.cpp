fdd9eb605624257429cc988a39c2d2b711b6f8b2
dfvargasmxo <kiriarteecuador@gmail.com>
2026-08-24 13:20:53 +0000

    semana2sesion1

--- laboratorios/semana-02-sesion-01/ejercicio1_class_rectangulo.cpp
+++ laboratorios/semana-02-sesion-01/ejercicio1_class_rectangulo.cpp
@@ -29,24 +29,34 @@ public:
     double getAltura() { return altura; }
 
     bool setBase(double nuevaBase) {
+        if (nuevaBase <= 0) {
+
+            base = nuevaBase;
+            return true;
+        }
         // TODO: si nuevaBase no es positiva, devuelve false sin modificar base.
         // Si es positiva, asigna base = nuevaBase y devuelve true.
         return false;
     }
 
     bool setAltura(double nuevaAltura) {
+        if (nuevaAltura <= 0) {
+            return false;
+        } 
+        altura = nuevaAltura;
+        return true;
         // TODO: mismo patron que setBase(), pero para altura.
-        return false;
+        
     }
 
     double area() {
         // TODO: retorna base * altura
-        return 0.0;
+        return base * altura;
     }
 
     double perimetro() {
         // TODO: retorna 2 * (base + altura)
-        return 0.0;
+        return 2 * (base + altura);
     }
 };
 
--- laboratorios/semana-02-sesion-01/ejercicio3_rango_notas.cpp
+++ laboratorios/semana-02-sesion-01/ejercicio3_rango_notas.cpp
@@ -30,16 +30,21 @@ public:
     double getMaxima() { return maxima; }
 
     bool setRango(double nuevaMinima, double nuevaMaxima) {
+        if ((nuevaMinima < 0) || (100 < nuevaMaxima) || (nuevaMaxima < nuevaMinima)) { return false;} else {
+        minima = nuevaMinima;
+        maxima = nuevaMaxima;
+        return true;
+        }
         // TODO: si nuevaMinima es menor que 0, o nuevaMaxima es mayor que
         // 100, o nuevaMinima es mayor que nuevaMaxima, devuelve false sin
         // modificar minima ni maxima. Si las tres condiciones son
         // validas, asigna minima y maxima, y devuelve true.
-        return false;
     }
 
     bool estaEnRango(double nota) {
+        if (minima < nota < maxima) { return true; } else {
         // TODO: retorna true si nota esta entre minima y maxima (incluidos).
-        return false;
+        return false; }
     }
 };
 
